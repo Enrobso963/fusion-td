cd02ab55339c7681865b16e452064d4e512c3aa7
Louis <LRJOsborne@gmail.com>
2026-07-05 21:57:31 +0100

    Add damage resolver: physical/magic armour system

--- sanbox.gd
+++ sanbox.gd
@@ -15,13 +15,31 @@ func _ready() -> void:
 	print("He could be found at coordinates ", spawn_pos, ".")
 	
 	hp = apply_damage(hp, arrow_damage)
-	print ("After ", enemy_name, " was struck by an arrow, his health was reduced to ", hp, ". OUCH!!")
+	print("After ", enemy_name, " was struck by an arrow, his health was reduced to ", hp, ". OUCH!!")
 	
-	var dps: float = 0
+	var dps: float = calculate_dps(arrow_damage, archer_firerate)
+	print("Those fine archers were dealing ", dps, " of damage a second!!!")
 	
-	dps = calculate_dps(arrow_damage, archer_firerate)
-	print ("Those fine archers were dealing ", dps, " of damage a second!!!")
+	var arrow_type: String = "physical"
+	var orb_type: String = "magic"
+	var orb_damage: int = 25
+	var igor_defence: String = "armoured"
+	var demon_defence: String = "warded"
+	var rat_defence: String = "unarmoured"
 	
+	var igor_arrow: int = resolve_damage(arrow_damage, arrow_type, igor_defence)
+	var igor_orb: int = resolve_damage(orb_damage, orb_type, igor_defence)
+	var demon_arrow: int = resolve_damage(arrow_damage, arrow_type, demon_defence)
+	var demon_orb: int = resolve_damage(orb_damage, orb_type, demon_defence)
+	var rat_arrow: int = resolve_damage(arrow_damage, arrow_type, rat_defence)
+	var rat_orb: int = resolve_damage(orb_damage, orb_type, rat_defence)
+	
+	print("An arrow does ", igor_arrow, " damage to Igor")
+	print("An orb does ", igor_orb, " damage to Igor")
+	print("An arrow does ", demon_arrow, " damage to Demons")
+	print("An orb does ", demon_orb, " damage to Demons")
+	print("An arrow does ", rat_arrow, " damage to Rats")
+	print("An orb does ", rat_orb, " damage to Rats")
 	
 func calculate_dps(tower_damage: int, attack_per_second: float) -> float:
 	var damage_per_second = tower_damage * attack_per_second
@@ -30,6 +48,16 @@ func calculate_dps(tower_damage: int, attack_per_second: float) -> float:
 func apply_damage(current_health: int, damage: int) -> int:
 	var remaining = current_health - damage
 	return remaining
+	
+func resolve_damage(damage: int, damage_type: String, defence: String) -> int:
+	var received: int
+	if defence == "armoured" and damage_type == "physical":
+		received = damage * 0.5
+	elif defence == "warded" and damage_type == "magic":
+		received = damage * 0.5
+	else:
+		received = damage
+	return received
 		
 func _process(delta: float) -> void:
 	pass
