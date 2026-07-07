edd957b00a13b2c30fea30fd6c0cc557c13ac975
Louis <LRJOsborne@gmail.com>
2026-07-07 17:11:58 +0100

    Spawn enemies on a timer via timeout signal

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -3,12 +3,12 @@ extends Node2D
 const ENEMY = preload("res://Scenes/enemy.tscn")
 
 @onready var path_2d: Path2D = $Path2D
-
+@onready var spawn_timer: Timer = $Timer
 
 func _ready() -> void:
+	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
+
+
+func _on_spawn_timer_timeout() -> void:
 	var enemy = ENEMY.instantiate()
 	path_2d.add_child(enemy)
-
-
-func _process(delta: float) -> void:
-	pass
